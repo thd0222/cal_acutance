7b0779d055d26835d643c637b9719a0932b05c7e
thd0222 <thdtjddb@naver.com>
2019-07-03 16:42:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,26 @@ from acutance import cal_acutance
 from option import args
 
 src = []
-names = []
 if os.path.isfile(args.src):
-    image = cv2.imread(args.src)
-    if image is not None:
-        src.append(image)
-        print(args.src)
-        names.append(os.path.basename(args.src))
+    src.append(args.src)
 elif os.path.isdir(args.src):
     files = os.listdir(args.src)
     for file in files:
-        path = os.path.join(args.src, file)
-        image = cv2.imread(path)
-        if image is not None:
-            src.append(image)
-            print(path)
-            names.append(file)
+        src.append(os.path.join(args.src, file))
 else:
     print('src error. :', args.src)
 
 f = open(args.result, 'w')
-for name, image in zip(names, src):
+max_val = 0.0
+max_name = None
+for path in src:
+    image = cv2.imread(path)
+    if image is None:
+        continue
+    name = os.path.basename(path)
     acu = cal_acutance(image)
     print(acu)
     f.write(name + '\t' + str(acu))
+    if max_val < acu:
+        max_val = acu
+        max_name = name
